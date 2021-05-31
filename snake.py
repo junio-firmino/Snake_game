48f4b1f67517f2e55acbabbb935adc39161e59be
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-05-31 06:57:14 -0300

    New files with changings in the code.

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from food import Food
 from snake_shape import Snake
+from snake_scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -26,7 +27,8 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    if snake.segment[0].distance(food) < 12:
+    if snake.segment[0].distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
